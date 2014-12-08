b044151f9cadceee8bcf5e04add7faad76d2c40b
masataka.takeuchi <masataka.takeuchi@l-is-b.com>
2014-12-08 20:30:42 +0900

    オフィスのクラウドサービスのユーティリティ群、使わないものもあるので遅延で初期化する。

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-module.exports = {
-  onedrive: require('./onedrive'),
-  gdrive: require('./googledrive'),
-  Word: require('./word'),
-  Excel: require('./excel')
+
+var props = {
+    'onedrive':  './onedrive',
+    'gdrive':    './googledrive',
+    'gcalendar': './googlecalendar',
+    'Word':      './word',
+    'Excel':     './excel'
 };
+
+var index = {};
+for (var name in props) {
+    Object.defineProperty(index, name, {
+        get: (function(path) { // delay
+            return function() { return require(path); };
+        })(props[name])
+    });
+}
+module.exports = index;
